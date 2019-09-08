c9c12a024595f33aa3dd6cccbcabbcfa0954dc7b
khay-EMMA <kalunjoku123@gmail.com>
2019-09-08 22:54:17 +0200

    initial commit

---
+++ public/App.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hey</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("products"));
